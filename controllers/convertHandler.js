ae40ba455c7a5cb9208573d18a6f5734b79b9cdf
Carlos Abraham Saldaña Enriquez <164584462+CarlosASE@users.noreply.github.com>
2024-09-07 22:43:57 +0000

    Cambios

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,36 +1,71 @@
+
+
 function ConvertHandler() {
   
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
+  this.getNum = function(input) {    
+    return Number(input);
   };
   
   this.getUnit = function(input) {
-    let result;
-    
-    return result;
+    const units = {
+      gal: "gal",
+      lbs: "lbs",
+      mi: "mi",
+      l: "l",
+      kg: "kg",
+      km: "km"
+    }    
+    return units[unit];
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+    initUnit =  initUnit.toLowerCase();    
+    return {
+      gal: "L",
+      lbs: "kg",
+      mi: "km",
+      l: "gal",
+      km: "mi",      
+    }[initUnit];
   };
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+  this.spellOutUnit = function(unit) {    
+    return {
+      gal: "gallon", 
+      lbs: "pound",
+      mi: "mile",
+      l: "liter",
+      kg: "kilogram",
+      km: "kilometer",
+    }[unit];
   };
   
   this.convert = function(initNum, initUnit) {
+    initUnit = initUnit.toLowercase();
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
-    
-    return result;
+    switch (initUnit) {
+      case "gal":
+        result = initNum * galToL;
+        break;
+      case "lbs":
+        result = initNum * lbsToKg;
+        break;
+      case "mi":
+        result = initNum * miToKm;
+        break;
+      case "l":
+        result = initNum / galToL;
+        break;
+      case "kg":
+        result = initNum / lbsToKg;
+        break;
+      case "km":
+        result = initNum / miToKm;
+        break;        
+    }    
+    return Number(XPathResult.toFixed(5));
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
